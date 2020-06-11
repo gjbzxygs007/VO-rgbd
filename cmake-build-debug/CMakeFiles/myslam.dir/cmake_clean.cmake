88f18d8c17d72f655b490c87d9190d19f74aa49d
gjbzxygs007 <jiananz1@uci.edu>
2020-06-10 23:27:25 -0700

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/myslam.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myslam.dir/main.cpp.o"
+  "myslam"
+  "myslam.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myslam.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
